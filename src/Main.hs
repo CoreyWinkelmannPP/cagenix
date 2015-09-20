fa0745baf58c676cbc4291f5139956488699dbf3
Corey Winkelmann <corey.winkelmann@cagenix.net>
2015-09-20 10:44:28 -0600

    Initial endpoint.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import Web.Scotty
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  scotty 3000 $ do
+    get "/" $ do
+      text "hello!"
+    get "/hello" $ do
+      text "hello world!"
