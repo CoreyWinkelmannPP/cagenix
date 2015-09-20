4249adde94ba1bda5c1cb336758f0fd19ca445a5
Corey Winkelmann <corey.winkelmann@cagenix.net>
2015-09-20 11:06:57 -0600

    Example of json output.

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,40 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Main where
 
+import Data.Monoid ((<>))
+import Data.Aeson (FromJSON, ToJSON)
+import GHC.Generics
 import Web.Scotty
 
-main :: IO ()
+data User = User { userId :: Int, userName :: String } deriving (Show, Generic)
+instance ToJSON User
+instance FromJSON User
+
+bob :: User
+bob = User { userId = 1, userName = "bob" }
+
+jenny :: User
+jenny = User { userId = 2, userName = "jenny" }
+
+allUsers :: [User]
+allUsers = [bob, jenny]
+
+matchesId :: Int -> User -> Bool
+matchesId id user = userId user == id
+
 main = do
   scotty 8080 $ do
-    get "/" $ do
-      text "hello!"
-    get "/hello" $ do
-      text "hello world!"
+    get "/" $ text "Welcome"
+
+    get "/hello/:name" $ do
+      name <- param "name"
+      text ("hello " <> name <> "!")
+
+    get "/users" $ do
+      json allUsers
+
+    get "/users/:id" $ do
+      id <- param "id"
+      json (filter (matchesId id) allUsers)
